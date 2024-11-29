53c77599262146c88c7ce3bef3bd1c624da10d9f
pawel.primus <pawel.primus@trapezegroup.pl>
2024-11-29 18:02:48 +0100

    Init code

---
+++ src/main/java/DAY_0X/DAY_0X_1.java
@@ -0,0 +1,19 @@
+package DAY_0X;
+
+import READER.FileReader;
+import READER.InputType;
+
+public class DAY_0X_1 {
+
+    private static final String DAY = "0X";
+
+    public static void main(String[] args) throws Exception {
+
+        String[] input = FileReader.readFileAsString(DAY, InputType.TEST).split("[\\r\\n]+");
+        String result = "";
+
+        System.out.println("RESULT: " + result);
+
+
+    }
+}
